4dae2d29d4d070aeb9a0688adb07db565f3d474e
MEKuroko007 <essalhimu@gmail.com>
2023-09-20 15:11:43 +0200

    check

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * main - Entry point for Monty interpreter
  * @argc: Number of command-line arguments
@@ -17,26 +16,26 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (fgets(line, sizeof(line), file))
 	{
 		char *opcode = strtok(line, " \n\t");
 		char *argument = strtok(NULL, " \n\t");
 
 		if (opcode == NULL)
-		{
-			continue;
-		}
+		{ continue; }
 		if (strcmp(opcode, "push") == 0)
 		{
 			_push(&stack, line_number, argument);
+		}
+		else if (strcmp(opcode, "#") == 0)
+		{
+			continue;
 		}
 		else
 		{
