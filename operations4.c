b0e6c7fbc2d2001b1cd03a535116327827c1fcf7
MEKuroko007 <essalhimu@gmail.com>
2023-09-20 17:16:44 +0200

    check

--- operations4.c
+++ operations4.c
@@ -48,28 +48,30 @@ void rotl(stack_t **stack, unsigned int line_number)
 	}
 }
 /**
- * rotr - rotates the stack to the bottom
+ * rotr - rotates the stack to the bottom.
  * @stack: pointer to the stack
  * @line_number: line number in the file
  */
 void rotr(stack_t **stack, unsigned int line_number)
 {
+	stack_t *last;
 	(void)line_number;
 
-	if (*stack != NULL && (*stack)->next != NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		stack_t *last = *stack;
-
-		while (last->next != NULL)
-		{
-			last = last->next;
-		}
-
-		last->prev->next = NULL;
-		last->prev = NULL;
-		last->next = *stack;
-		(*stack)->prev = last;
-		*stack = last;
+		return;
 	}
-}
 
+	last = *stack;
+
+	while (last->next)
+	{
+		last = last->next;
+	}
+
+	last->next = *stack;
+	last->prev->next = NULL;
+	last->prev = NULL;
+	(*stack)->prev = last;
+	(*stack) = last;
+}
