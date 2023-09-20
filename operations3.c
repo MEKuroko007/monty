a5998107c5c4499337fab20a944faf55e6adc925
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-09-20 22:50:04 +0100

    check some cases now

--- operations3.c
+++ operations3.c
@@ -133,6 +133,8 @@ void pchar(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(stack);
+		fclose(file);
 		exit(EXIT_FAILURE);
 	}
 
@@ -141,11 +143,12 @@ void pchar(stack_t **stack, unsigned int line_number)
 	if (ascii_value < 0 || ascii_value > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_stack(stack);
+		fclose(file);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("%c", ascii_value);
-	printf("\n");
+	printf("%c\n", ascii_value);
 
 	temp = *stack;
 	*stack = (*stack)->next;
